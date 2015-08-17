83df8a91d161a8bfe3f463c451a30828ac86a178
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2015-08-17 11:34:00 +0300

    backend & front end code

---
+++ static/app/diagram.js
@@ -0,0 +1,106 @@
+var $ = require('jquery');
+var d3 = require('../../node_modules/d3/d3.min');
+var sankeySetup = require('./sankey');
+var renderTimeline = require('./timeline');
+
+sankeySetup(d3);
+
+module.exports = (options) => {
+  options.target.innerHTML = '';
+
+  var widthForItem = 422;
+
+  var margin = {top: 1, right: 1, bottom: 6, left: 1},
+      width = (widthForItem * (options.labels.length - 1)) - margin.left - margin.right,
+      height = 660 - margin.top - margin.bottom;
+
+  renderTimeline({
+    target: options.timelineTarget,
+    labels: options.labels,
+    width: widthForItem * ( options.labels.length - 1 ) + 100,
+    itemWidth: widthForItem - 2,
+    events: options.events
+  });
+
+  var formatNumber = d3.format(",.0f"),
+      format = function(d) { return formatNumber(d) + " TWh"; },
+      color = d3.scale.category20();
+
+  var sankey = d3.sankey()
+      .nodeWidth(15)
+      .nodePadding(10)
+      .size([width, height]);
+
+  var path = sankey.link();
+
+  d3.json(options.data, function(topics) {
+
+    var svg = d3.select(options.target).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    sankey
+        .nodes(topics.nodes)
+        .links(topics.links)
+        .layout(32);
+
+    var link = svg.append("g").selectAll(".link")
+        .data(topics.links)
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("d", path)
+        .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+        .sort(function(a, b) { return b.dy - a.dy; });
+
+    link.append("title")
+        .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+    link
+      .on('mouseover', function(d){
+        d3.select(this).style('stroke', d.source.color);
+      })
+      .on('mouseout', function(d){
+        d3.select(this).style('stroke', 'black');
+      });
+
+    var node = svg.append("g").selectAll(".node")
+        .data(topics.nodes)
+      .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .call(d3.behavior.drag()
+        .origin(function(d) { return d; })
+        .on("dragstart", function() { this.parentNode.appendChild(this); })
+        .on("drag", dragmove))
+        .on("dblclick", function(){
+          console.log(d3.select(this))
+        });
+
+    node.append("rect")
+        .attr("height", function(d) { return d.dy; })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+        .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+      .append("title")
+        .text(function(d) { return d.name + "\n" + format(d.value); });
+
+    node.append("text")
+        .attr("x", -6)
+        .attr("y", function(d) { return d.dy / 2; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .attr("transform", null)
+        .text(function(d) { return d.name; })
+      .filter(function(d) { return d.x < width / 2; })
+        .attr("x", 6 + sankey.nodeWidth())
+        .attr("text-anchor", "start");
+
+    function dragmove(d) {
+      d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+      sankey.relayout();
+      link.attr("d", path);
+    }
+  });
+}
---
+++ static/app/timeline.js
@@ -0,0 +1,23 @@
+var handlebars = require('handlebars');
+var $ = require('jquery');
+
+var templateHTML = `
+  <ul class="timeline">
+    {{#each labels}}
+      <li class="timeline-item">
+        <a href="#/topics/{{this}}">{{this}}</a>
+      </li>
+    {{/each}}
+  </ul>
+`;
+
+var template = handlebars.compile(templateHTML);
+
+module.exports = (options) => {
+  options.target.innerHTML = template({ labels: options.labels });
+
+  var $ul = $(options.target).find('ul');
+
+  $ul.css('width', `${options.width}px`);
+  $ul.find('.timeline-item').css('width', `${options.itemWidth}px`);
+};
